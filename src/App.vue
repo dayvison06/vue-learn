fefeb48534dfe057e64da470bcb6fd67c395919e
dayvison06 <dayvison.rocha@hotmail.com>
2024-08-01 14:29:14 -0300

    Entendendo diretivas e variaveis locais

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <PageHeader></PageHeader>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <PageHeader
+      v-if="showHeader"
+  />
+
+  <div
+      v-for="obj in todos"
+      v-bind:key="obj.id"
+      class="tomilho"
+  >
+    {{ obj.title }}
+  </div>
+
+  <img
+      alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -13,17 +25,65 @@ export default {
   components: {
     HelloWorld,
     PageHeader
+  },
+  // Central de variaveis do componente
+  data(){
+    return{
+      showHeader: true,
+
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        }
+      ]
+
+      }
+    }
   }
-}
 </script>
 
 <style>
+.tomilho{
+  background-color: #000;
+  margin: 5px 50px 5px 50px;
+  padding: 3px 6px;
+  color: #fff;
+  text-align: left;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px;
 }
 </style>
